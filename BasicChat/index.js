e19fa26ca160c6940f91913da1486c2d8de20f4c
Parmendra-Panwar <panwparmendra7@gmail.com>
2026-02-13 01:13:46 +0530

    GEN AI Mentor BABA 13/02/2025

--- BasicChat/index.js
+++ BasicChat/index.js
@@ -1,48 +1,38 @@
 import { GoogleGenAI } from "@google/genai";
-import readlineSync  from "readline-sync";
+import readlineSync from 'readline-sync';
 
-const ai = new GoogleGenAI({apiKey: "AIzaSyAkunxxp_PaHGoIuiriNAewGioNfrISmH0"});
-var readline = require('linebyline');
+const ai = new GoogleGenAI({ apiKey: "" });
 
-const History = [
-    {
-        role: "user",
-        parts: [{ text: "Hello" }],
-    },
-    {
-        role: "model",
-        parts: [{ text: "Great to meet you. What would you like to know?" }],
-    },
-];
+const History = [];
 
-let response;
-async function main(Contents) {
-  response = await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
-    history: History,
-    contents: Contents,
+async function mentor(userProblem) {
+    History.push({
+        role: 'user',
+        parts:[{text: userProblem}]
+    })
 
-    config: {
-      systemInstruction: "You are a cat. Your name is Neko.",
-    },
-  });
-  console.log(response.text);
+    const response = await ai.models.generateContent({
+        model: "gemini-3-flash-preview",
+        history: History,
+        contents: userProblem,
+
+        config: {
+            systemInstruction: "You are a monk.",
+        },
+    });
+
+    History.push({
+        role: 'model',
+        parts: [{test: response.text}]
+    })
+    console.log("\n");
+    console.log(response.text);
 }
 
-const jadu = async () => {
-    var  rl = readline('./somefile.txt');
-
-    const udata = {
-        role: "user",
-        parts: [{text: ""}]
-    };
-    const mdata = {
-        role: "user",
-        parts: [{text: ""}]
-    };
-    await main();
-    jadu();
+async function main(){
+   const userProblem = readlineSync.question("Ask me anything--> ");
+   await mentor(userProblem);
+   main();
 }
 
-await jadu();
-
+main();
